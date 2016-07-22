e0409fd879d942aa9468b4cf0fabd2208ea1211d
Britney <britney.presley26@gmail.com>
2016-07-22 14:56:17 -0700

    problem 3 finished

--- app.js
+++ app.js
@@ -54,11 +54,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var output1 = sum(sum(a,b)[0],c)[0];
+  var output2 = multiply(multiply(a,b)[0],c)[0];
+  var message1 = a + ' and ' + b + ' and ' + c + ' sum to ' + output1 + '.';
+  var message2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + output2 + '.';
+  console.log('4 and 7 and 5 sum to 16. is the expected output');
+  console.log('The product of 4 and 7 and 5 is 140. is the expected output.');
+  console.log[message1, message2 + 'is the actual output'];
+  return [output1, output2, message1, message2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
